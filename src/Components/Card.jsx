d3fa7b056afdf94023c66b524680daa1e9b1c0a8
ARYAN KHOCHARE <131617464+Aryankhochare@users.noreply.github.com>
2024-07-22 11:48:00 +0530

    Update Card.jsx

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,56 +1,10 @@
-// import React from "react";
-
-// const Card = ({ pokemon, loading, infoPokemon, lastPokemonElementRef }) => {
-//     return (
-//         <>
-//             {loading && pokemon.length === 0 ? (
-//                 <h1>Loading...</h1>
-//             ) : (
-//                 pokemon.map((item, index) => {
-//                     if (pokemon.length === index + 1) {
-//                         return (
-//                             <div 
-//                                 className="card" 
-//                                 key={item.id} 
-//                                 ref={lastPokemonElementRef}
-//                                 onClick={() => infoPokemon(item)}
-//                             >
-//                                 <h2>{item.id}</h2>
-//                                 <img src={item.sprites.front_default} alt={item.name} />
-//                                 <h2>{item.name}</h2>
-//                             </div>
-//                         );
-//                     } else {
-//                         return (
-//                             <div 
-//                                 className="card" 
-//                                 key={item.id} 
-//                                 onClick={() => infoPokemon(item)}
-//                             >
-//                                 <h2>{item.id}</h2>
-//                                 <img src={item.sprites.front_default} alt={item.name} />
-//                                 <h2>{item.name}</h2>
-//                             </div>
-//                         );
-//                     }
-//                 })
-//             )}
-//         </>
-//     );
-// };
-
-// export default Card;
-
-
-
-
 import React, { useState } from "react";
 
 const Card = ({ pokemon, loading, infoPokemon, lastPokemonElementRef }) => {
     const [playingAudio, setPlayingAudio] = useState(null);
 
     const playSound = (id, event) => {
-        event.stopPropagation(); // Prevent the card's onClick from firing
+        event.stopPropagation(); 
         if (playingAudio) {
             playingAudio.pause();
             playingAudio.currentTime = 0;
